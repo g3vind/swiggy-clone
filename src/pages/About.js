2b29bfd60e1cb45edeb37c9298bd40e3ab580814
g3vind <g3vind@gmail.com>
2023-12-29 15:53:22 +0530

    about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,9 +1,106 @@
-const About = () => {
-  return (
-    <>
-      <h1>Hi</h1>
-    </>
-  );
-};
+"use client";
 
-export default About;
+import React from "react";
+import { MapPin } from "lucide-react";
+
+const locations = [
+  {
+    title: "Bengaluru office",
+    timings: "Mon-Sat 9am to 5pm.",
+    address: "100, Electronic City Phase-1, Bengaluru, Karnataka 560100 IN",
+  },
+  {
+    title: "Head office",
+    timings: "Mon-Sat 9am to 5pm.",
+    address: "12th Main Rd, Indiranagar, Bengaluru, Karnataka 560008 IN",
+  },
+  {
+    title: "Karnataka office",
+    timings: "Mon-Sat 9am to 5pm.",
+    address:
+      "42, Residency Rd, Shanthala Nagar, Ashok Nagar, Bengaluru, Karnataka 560025 IN",
+  },
+];
+
+export default function About() {
+  const [isMenuOpen, setIsMenuOpen] = React.useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  return (
+    <div>
+      <div className="mx-auto -mt-14 max-w-7xl px-4">
+        {/* Hero Map */}
+        <div className="flex flex-col space-y-8 pb-10 pt-12 md:pt-24">
+          <p className="text-2xl font-bold text-gray-900 md:text-3xl md:leading-10">
+            Made with love, right here in India
+          </p>
+          {/* <p className="max-w-4xl text-base text-gray-600 md:text-xl">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore
+            veritatis voluptates neque itaque repudiandae sint, explicabo
+            assumenda quam ratione placeat?
+          </p> */}
+        </div>
+        <div className="w-full -mt-6 space-y-4">
+          <img
+            className="h-[170px] w-full rounded-xl object-cover md:h-full"
+            src="https://dev-ui-image-assets.s3.ap-south-1.amazonaws.com/google-map.jpg"
+            alt=""
+          />
+        </div>
+        {/* locations */}
+        <div className="my-6 flex flex-col gap-y-6 md:flex-row lg:justify-around">
+          {locations.map((location) => (
+            <div
+              key={location.title}
+              className="flex flex-col space-y-3 md:w-2/4 lg:w-1/5"
+            >
+              <MapPin className="h-5 w-5" />
+              <p className="w-full text-xl font-semibold  text-gray-900">
+                {location.title}
+              </p>
+              <p className="w-full text-base text-gray-700">
+                {location.timings}
+              </p>
+              <p className="text-sm font-medium">{location.address}</p>
+            </div>
+          ))}
+        </div>
+        <hr className="mt-20" />
+
+        {/* Hiring Banner */}
+        <div className="flex flex-col items-center gap-x-4 gap-y-4 py-16 md:flex-row">
+          <div className="space-y-6">
+            <p className="text-sm font-semibold md:text-base">
+              Join our team &rarr;
+            </p>
+            <p className="text-3xl font-bold md:text-4xl">
+              We&apos;re just getting started
+            </p>
+            <p className="text-base text-gray-600 md:text-lg">
+              Our philosophy is simple — hire a team of diverse, passionate
+              people and foster a culture that empowers you to do your best
+              work.
+            </p>
+            <button
+              type="button"
+              className="rounded-md bg-black px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-black/80 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-black"
+            >
+              Join Now
+            </button>
+          </div>
+          <div className="md:mt-o mt-10 w-full">
+            <img
+              src="https://images.unsplash.com/photo-1605165566807-508fb529cf3e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2340&q=80"
+              alt="Getting Started"
+              className="rounded-lg"
+            />
+          </div>
+        </div>
+      </div>
+      <hr className="mt-6" />
+    </div>
+  );
+}
