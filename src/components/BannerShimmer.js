2259702f79915b9df70c3207889ad63ff61efdc9
g3vind <g3vind@gmail.com>
2023-12-29 18:00:17 +0530

    spinner

--- src/components/BannerShimmer.js
+++ src/components/BannerShimmer.js
@@ -1,8 +1,19 @@
+import { SpinnerCircularFixed } from "spinners-react";
 function BannerShimmer() {
   return (
-    <div className="flex">
+    <div className="flex flex-col items-center justify-center">
       <h2 className="banner-shimmer banner-text text-center items-center justify-center text-xl pt-6">
         Looking for great food near you!
+        <span>
+          <SpinnerCircularFixed
+            className="text-center items-center spinner"
+            size={60}
+            thickness={100}
+            speed={100}
+            color="black"
+            secondaryColor="rgba(0, 0, 0, 0.44)"
+          />
+        </span>
       </h2>
     </div>
   );
