a653947ced0b25c13dd57ae86610c80a98a2a5a8
g3vind <g3vind@gmail.com>
2023-12-29 15:47:05 +0530

    upgraded react router to v6

--- src/App.js
+++ src/App.js
@@ -5,31 +5,16 @@ import Body from "./components/Body";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
 import Cart from "./pages/Cart";
-import {
-  createBrowserRouter,
-  BrowserRouter,
-  Routes,
-  Route,
-  RouterProvider,
-} from "react-router-dom";
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
 import { Footer } from "./components/Footer";
 import { Error } from "./pages/Error";
 
 const AppLayout = () => {
   return (
-    // <BrowserRouter>
-    //   <Header />
-    //   <Routes>
-    //     <Route path="/" element={<Body />} />
-    //     <Route path="/about" element={<About />} />
-    //     <Route path="/cart" element={<Cart />} />
-    //     <Route path="/contact" element={<Contact />} />
-    //   </Routes>
-    //   <Footer />
-    // </BrowserRouter>
     <div className="app">
       <Header />
-      <Body />
+      <Outlet />
+      <Footer />
     </div>
   );
 };
@@ -38,21 +23,24 @@ const appRouter = createBrowserRouter([
   {
     path: "/",
     element: <AppLayout />,
-    errorElement: <Error />,
-  },
-  {
-    path: "/about",
-    element: <About />,
-    errorElement: <Error />,
-  },
-  {
-    path: "/contact",
-    element: <Contact />,
-    errorElement: <Error />,
-  },
-  {
-    path: "/cart",
-    element: <Cart />,
+    children: [
+      {
+        path: "/",
+        element: <Body />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
+      },
+      {
+        path: "/cart",
+        element: <Cart />,
+      },
+    ],
     errorElement: <Error />,
   },
 ]);
