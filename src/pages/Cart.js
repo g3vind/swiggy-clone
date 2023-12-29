2b34ff59495082822703dffeb087ff341341653f
g3vind <g3vind@gmail.com>
2023-12-29 16:34:55 +0530

    cart page

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,7 +1,16 @@
 import React from "react";
 
-function Cart() {
-  return <div>Cart</div>;
+export default function Cart() {
+  return (
+    <section>
+      <div className="px-2 lg:flex lg:flex-row lg:items-center">
+        <div className="w-full lg:w-1/2">
+          <img
+            src="https://i.pinimg.com/originals/2e/ac/fa/2eacfa305d7715bdcd86bb4956209038.png"
+            className="h-full w-full rounded-md object-cover ml-80 mt-16"
+          />
+        </div>
+      </div>
+    </section>
+  );
 }
-
-export default Cart;
