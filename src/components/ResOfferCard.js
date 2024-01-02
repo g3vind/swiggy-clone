6a5a81ded53f9dc6dec128023889ed3aa334e8b2
g3vind <g3vind@gmail.com>
2024-01-02 21:21:30 +0530

    res menu ui fix partial

--- src/components/ResOfferCard.js
+++ src/components/ResOfferCard.js
@@ -5,11 +5,6 @@ export default function ResOfferCard({ offers }) {
   return (
     <div className="inline-block rounded-md border p-3 text-xs">
       <div className="text-sm xs:text-xs font-bold text-zinc-600 whitespace-nowrap mb-1 flex items-center gap-2">
-        <img
-          className="w-5 xs:w-3 xs:text-xs"
-          src={IMG_CDN_URL + logo}
-          alt=""
-        />{" "}
         <span>{header}</span>
       </div>
       <div className="whitespace-nowrap xs:text-xs font-bold text-gray-400">
